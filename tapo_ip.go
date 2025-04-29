b4af7bdf8639b85b1b48998f6940ec78d3aacc9f
StuMP90 <gitstm90@dsv1.co.uk>
2025-04-29 18:07:14 +0100

    Update tapo_ip.go

--- tapo_ip.go
+++ tapo_ip.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
+
 	"github.com/pelletier/go-toml"
 )
 
 type Camera struct {
-	Name     string
-	IP       string
-	Username string
-	Password string
-	RTSPPath string
-	RTSPUrl  string
+	Name       string
+	IP         string
+	Username   string
+	Password   string
+	RTSPPath   string
+	LQRTSPPath string
+	RTSPUrl    string
 }
 
 type tapoCamerasTOML struct {
 	Cameras []struct {
-		IP       string `toml:"ip"`
-		Username string `toml:"username"`
-		Password string `toml:"password"`
-		RTSPPath string `toml:"rtsp_path"`
+		IP         string `toml:"ip"`
+		Username   string `toml:"username"`
+		Password   string `toml:"password"`
+		RTSPPath   string `toml:"rtsp_path"`
+		LQRTSPPath string `toml:"lq_rtsp_path"`
 	} `toml:"tapo_cameras"`
 }
 
@@ -41,13 +44,18 @@ func readTapoCameras(filename string) []Camera {
 	}
 	var cams []Camera
 	for _, c := range conf.Cameras {
+		lqPath := c.LQRTSPPath
+		if lqPath == "" {
+			lqPath = c.RTSPPath
+		}
 		cam := Camera{
-			Name:     "Tapo Camera",
-			IP:       c.IP,
-			Username: c.Username,
-			Password: c.Password,
-			RTSPPath: c.RTSPPath,
-			RTSPUrl:  fmt.Sprintf("rtsp://%s:%s@%s:554%s", c.Username, c.Password, c.IP, c.RTSPPath),
+			Name:       "Tapo Camera",
+			IP:         c.IP,
+			Username:   c.Username,
+			Password:   c.Password,
+			RTSPPath:   c.RTSPPath,
+			LQRTSPPath: lqPath,
+			RTSPUrl:    fmt.Sprintf("rtsp://%s:%s@%s:554%s", c.Username, c.Password, c.IP, c.RTSPPath),
 		}
 		cams = append(cams, cam)
 	}
